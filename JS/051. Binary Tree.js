b8f36e57593a61fa0d304cc36c6da179bdb15908
Sayantan <sayantanbharati611@gmail.com>
2025-10-03 23:59:19 +0530

    15+ binary tree questions

--- JS/051. Binary Tree.js
+++ JS/051. Binary Tree.js
@@ -110,13 +110,56 @@ class Solution {
 
 
 /* leetcode: 236. Lowest Common Ancestor of a Binary Tree*/
-
+var lowestCommonAncestor = function(root, p, q) {
+    if(!root || root===p || root===q)return root
+    let left=lowestCommonAncestor(root.left, p, q)
+    let right=lowestCommonAncestor(root.right, p, q)
+    if(left && right )return root
+    return left?left:right
+};
 
 
 /* leetcode: 437. Path Sum III*/
+var pathSum = function(root, targetSum) {
+    let count=0
+    let map=new Map()
+    map.set(0,1)
+    function dfs(node,sum){
+        if(!node)return
 
+        sum+=node.val
+        count+=map.get(sum-targetSum)||0
+
+        map.set(sum,(map.get(sum)||0)+1)
+        
+        dfs(node.left, sum)
+        dfs(node.right, sum)
+        map.set(sum,map.get(sum)-1)
+    }
+    dfs(root,0)
+    return count
+};
 
 
 /* leetcode: 105. Construct Binary Tree from Preorder and Inorder Traversal*/
+var buildTree = function(preorder, inorder) {
+    let preIdx=0
+    let inMap=new Map()
+    for(let i=0;i<inorder.length;i++){
+        inMap.set(inorder[i],i)
+    }
+    function solve(inStart,inEnd){
+        if(inStart>inEnd)return null
 
+        let rootVal=preorder[preIdx++]
+        let root=new TreeNode(rootVal)
 
+        let inIdx=inMap.get(rootVal)
+
+        root.left=solve(inStart,inIdx-1)
+        root.right=solve(inIdx+1,inEnd)
+
+        return root
+    }
+    return solve(0,inorder.length-1)
+};
